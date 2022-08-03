24abceb78426fda2761bccaee4d2f74313e48d7b
cetvrtak <59363714+cetvrtak@users.noreply.github.com>
2022-08-03 13:52:39 +0200

    Add "Edit" button functoinality

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ const contacts = [
   {
     firstName: "Patrick",
     lastName: "Mahomes",
-    email: "spatmahomes@gmail.com",
+    email: "patmahomes@gmail.com",
     phone: "+901234567",
   },
   {
@@ -22,10 +22,22 @@ const contacts = [
 ];
 
 const modalAddEl = document.querySelector(".modal-add");
+const addFirstNameEl = document.querySelector("#add-firstname");
+const addLastNameEl = document.querySelector("#add-lastname");
+const addEmailEl = document.querySelector("#add-email");
+const addPhoneEl = document.querySelector("#add-phone");
+
+const modalEditEl = document.querySelector(".modal-edit");
+const editFirstNameEl = document.querySelector("#edit-firstname");
+const editLastNameEl = document.querySelector("#edit-lastname");
+const editEmailEl = document.querySelector("#edit-email");
+const editPhoneEl = document.querySelector("#edit-phone");
+
 const overlayEl = document.querySelector(".overlay");
 const addNewContact = document.querySelector(".add-contact");
 const contactsEl = document.querySelector(".contacts");
 const btnAdd = document.querySelector(".btn-add");
+const btnEdit = document.querySelector(".btn-edit");
 
 // Functions
 function insertContactHTML(el, i) {
@@ -41,7 +53,7 @@ function insertContactHTML(el, i) {
               <button class="btn open">
                   <ion-icon name="open-outline"></ion-icon>
               </button>
-              <button class="btn edit">
+              <button class="btn edit" data-id="${i}">
                   <ion-icon name="pencil-outline"></ion-icon>
               </button>
               <button class="btn delete">
@@ -58,36 +70,57 @@ function displayContacts() {
 displayContacts();
 
 function openModal() {
-  modalAddEl.classList.remove("hidden");
+  this.classList.remove("hidden");
   overlayEl.classList.remove("hidden");
 }
 function closeModal() {
   modalAddEl.classList.add("hidden");
+  modalEditEl.classList.add("hidden");
   overlayEl.classList.add("hidden");
 }
 function addContact(newContact) {
   contacts.push(newContact);
-  //   localStorage.setItem(localStorage, newContact);
   insertContactHTML(newContact, contacts.length - 1);
 }
-// function updateStorage() {
-//   contacts.forEach((el, i) => {
-//     localStorage.setItem(i, el);
-//   });
-// }
-// updateStorage();
+function saveContact(id) {
+  contacts[id].firstName = editFirstNameEl.textContent;
+  contacts[id].lastName = editLastNameEl.textContent;
+  contacts[id].email = editEmailEl.textContent;
+  contacts[id].phone = editPhoneEl.textContent;
+}
 
 // Event listeners
-addNewContact.addEventListener("click", openModal);
+addNewContact.addEventListener("click", openModal.bind(modalAddEl));
 overlayEl.addEventListener("click", closeModal);
 btnAdd.addEventListener("click", (e) => {
   e.preventDefault();
   closeModal();
   const newContact = {
-    firstName: document.querySelector("#firstname").value,
-    lastName: document.querySelector("#lastname").value,
-    email: document.querySelector("#email").value,
-    phone: document.querySelector("#phone").value,
+    firstName: addFirstNameEl.value,
+    lastName: addLastNameEl.value,
+    email: addEmailEl.value,
+    phone: addPhoneEl.value,
   };
   addContact(newContact);
+});
+
+let id;
+contactsEl.addEventListener("click", function (e) {
+  e.preventDefault();
+  const editBtn = e.target.closest(".edit");
+  if (editBtn) {
+    openModal.call(modalEditEl);
+
+    id = editBtn.dataset.id;
+    editFirstNameEl.value = contacts[id].firstName;
+    editLastNameEl.value = contacts[id].lastName;
+    editEmailEl.value = contacts[id].email;
+    editPhoneEl.value = contacts[id].phone;
+  }
+});
+
+btnEdit.addEventListener("click", function (e) {
+  e.preventDefault();
+  closeModal();
+  saveContact(id);
 });
